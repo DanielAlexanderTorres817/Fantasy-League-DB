6c5730a737aa461f8c8b510e107fcc726f73c5e7
Daniel Torres <DanielAlexanderTorres817@gmail.com>
2024-11-27 19:06:44 -0500

    added landing, login, and registration

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from views import views
+#from flask_sqlalchemy import SQLAlchemy
+#from models import db
+
+app = Flask(__name__)
+
+app.register_blueprint(views, url_prefix = "/")
+
+#DATABASE CODE HERE
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"  # SQLite database file
+#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # Disable unnecessary overhead
+
+
+#db.init_app(app)
+
+# with app.app_context():
+#     db.create_all()
+#     #print("success!")
+
+if __name__ == "__main__":
+    app.run(debug=True, port = 8000)
